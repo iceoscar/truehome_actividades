bc192cdaa6e62681175f061b7a062ada65d066ef
Oscar Daniel Martinez Corona <icedanielmc@gmail.com>
2021-12-23 12:45:11 -0600

    Se agrego la acción de cancelar actividad

--- src/activities/views.py
+++ src/activities/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Property, Activity, Survery
 from .serializers import PropertySerializer, ActivitySerializer, SurverySerializer
@@ -23,6 +25,18 @@ class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['get'], name='Cancelar Actividad', permission_classes=[permissions.IsAuthenticated])
+    def set_cancelled(self, request, pk=None):
+        activity = self.get_object()
+        activity.status = Activity.STATUS_CANCELLED
+        activity.save()
+        return Response({
+            'activity': activity.title,
+            'property': activity.property.title,
+            'schedule': activity.schedule,
+            'msg': 'Actividad Cancelada'
+        })
 
 
 class SurveryViewSet(viewsets.ModelViewSet):
