48101b03e42c2e064064b4eb87f67992f89c7cd1
Oscar Daniel Martinez Corona <icedanielmc@gmail.com>
2021-12-22 18:42:43 -0600

    instalación del paquete de restframework de Django; fix: downgrade de psycopg2-binary version 2.9 a 2.8

--- src/_project/settings.py
+++ src/_project/settings.py
@@ -38,7 +38,9 @@ PROJECT_APPS = [
     'activities',
 ]
 
-EXTRA_APPS = []
+EXTRA_APPS = [
+    'rest_framework',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -58,6 +60,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 ROOT_URLCONF = '_project.urls'
 
